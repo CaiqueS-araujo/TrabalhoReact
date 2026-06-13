bedc2e6974743c1e8f8ff14755b977d5d7c8be6c
DouglasCGJ <douglascostagomesdejesus@gmail.com>
2026-06-12 21:48:02 -0300

    Mudanças nas rotas para começar a pokedex

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import { Routes, Route } from "react-router-dom";
 import MyTeam from "./pages/MyTeam";
 import Login from "./pages/Login/index"
+import Pokedex from "./pages/Pokedex/index";
 
 function App() {
   return (    
     <Routes>
       <Route path="/meu-time" element={<MyTeam />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/pokedex" element={<Pokedex />} />
     </Routes>
   );
 }
