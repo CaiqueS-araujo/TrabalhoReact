fdef122dac88a1bd428a9ee981c265ec2bc37212
DouglasCGJ <douglascostagomesdejesus@gmail.com>
2026-06-15 17:45:54 -0300

    Melhoria da pokedex

--- src/pages/Pokedex/index.jsx
+++ src/pages/Pokedex/index.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { searchPokemon } from "../../services/pokeApi";
-import PokedexSearch from "../../components/Pokedex/Search";
-import PokedexDisplay from "../../components/Pokedex/Display";
+import PokedexSearch from "../../components/PokedexSearch";
+import PokedexDisplay from "../../components/PokedexDisplay";
 import styles from "./style.module.css";
 
 function Pokedex() {
@@ -12,16 +12,13 @@ function Pokedex() {
 
   const handleSearch = async (e) => {
     e.preventDefault(); 
-    
     if (!query.trim()) {
-      setError("Por favor, digite um nome ou número.");
+      setError("Digite um nome/número!");
       return;
     }
-
     setLoading(true);
     setError("");
     setPokemon(null);
-
     try {
       const data = await searchPokemon(query.toLowerCase());
       setPokemon(data);
@@ -36,21 +33,30 @@ function Pokedex() {
     <div className={styles.pokedexContainer}>
       <div className={styles.pokedexBody}>
         
-        <PokedexDisplay pokemon={pokemon} loading={loading} error={error} />
-
-        <div className={styles.infoScreen}>
-          {pokemon ? (
-             <div className={styles.infoContent}>
-               <h2>{pokemon.name.toUpperCase()}</h2>
-               <p><strong>Tipos:</strong> {pokemon.types.join(", ")}</p>
-               <p><strong>Movimentos:</strong> {pokemon.moves.join(", ")}</p>
-             </div>
-          ) : (
-            <p className={styles.systemText}>Aguardando pesquisa...</p>
-          )}
+        <div className={styles.leftPanel}>
+          <div className={styles.blueLens}></div>
+          <PokedexDisplay pokemon={pokemon} loading={loading} error={error} />
         </div>
 
-        <PokedexSearch query={query} setQuery={setQuery} handleSearch={handleSearch} />
+        <div className={styles.hinge}></div>
+
+        <div className={styles.rightPanel}>
+          
+          <div className={styles.infoScreen}>
+            {pokemon ? (
+               <div className={styles.infoContent}>
+                 <h2>{pokemon.name.toUpperCase()}</h2>
+                 <p><strong>TIPOS:</strong> {pokemon.types.join(", ")}</p>
+                 <p><strong>MOVS:</strong> {pokemon.moves.join(", ")}</p>
+               </div>
+            ) : (
+              <p className={styles.systemText}>AGUARDANDO...</p>
+            )}
+          </div>
+
+          <PokedexSearch query={query} setQuery={setQuery} handleSearch={handleSearch} />
+          
+        </div>
 
       </div>
     </div>
